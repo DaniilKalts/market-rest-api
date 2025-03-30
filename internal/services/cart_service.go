202500bd8b76f55fd7a735ffe5f71031cc3967c7
DaniilKalts <kaltsdaniil@gmail.com>
2025-03-30 07:45:28 +0500

    feat: add cart service

---
+++ internal/services/cart_service.go
@@ -0,0 +1,36 @@
+package services
+
+import (
+	"github.com/DaniilKalts/market-rest-api/internal/repositories"
+)
+
+type CartService interface {
+	AddItem(cartID int, itemID int) error
+	UpdateItem(cartID int, itemID int, quantity uint) error
+	DeleteItem(cartID int, itemID int) error
+	ClearCart(cartID int) error
+}
+
+type cartService struct {
+	repo repositories.CartRepository
+}
+
+func NewCartService(repo repositories.CartRepository) CartService {
+	return &cartService{repo: repo}
+}
+
+func (s *cartService) AddItem(cartID int, itemID int) error {
+	return s.repo.Add(cartID, itemID)
+}
+
+func (s *cartService) UpdateItem(cartID int, itemID int, quantity uint) error {
+	return s.repo.Update(cartID, itemID, quantity)
+}
+
+func (s *cartService) DeleteItem(cartID int, itemID int) error {
+	return s.repo.Delete(cartID, itemID)
+}
+
+func (s *cartService) ClearCart(cartID int) error {
+	return s.repo.Clear(cartID)
+}
